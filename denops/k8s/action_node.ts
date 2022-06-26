1c3f11bd14b548ba61e3d64072bda4f5ff536c83
skanehira <sho19921005@gmail.com>
2022-06-26 22:51:25 +0900

    refactor: add some columns in node list

--- denops/k8s/action_node.ts
+++ denops/k8s/action_node.ts
@@ -1,7 +1,80 @@
 import { batch, Denops, Table, vars } from "./deps.ts";
 import * as node from "./node.ts";
 import { IoK8sApiCoreV1Node } from "./models/IoK8sApiCoreV1Node.ts";
+import { IoK8sApiCoreV1NodeCondition } from "./models/IoK8sApiCoreV1NodeCondition.ts";
 import { Resource } from "./resource.ts";
+
+export function renderNodeStatus(node: IoK8sApiCoreV1Node): string {
+  const status: string[] = [];
+
+  const conditions: Record<string, IoK8sApiCoreV1NodeCondition> = {};
+  const nodeAllConditions = ["Ready"];
+  for (const cond of node.status?.conditions!) {
+    conditions[cond.type] = cond;
+  }
+
+  for (const validCondition of nodeAllConditions) {
+    const condition = conditions[validCondition];
+    if (!condition) {
+      continue;
+    }
+    if (condition.status === "True") {
+      status.push(condition.type);
+    } else {
+      status.push("Not" + condition.type);
+    }
+  }
+
+  if (status.length === 0) {
+    status.push("Unknown");
+  }
+  if (node.spec?.unschedulable) {
+    status.push("SchedulingDisabled");
+  }
+
+  return status.join(",");
+}
+
+const labelNodeRolePrefix = "node-role.kubernetes.io/";
+const nodeLabelRole = "kubernetes.io/role";
+
+export function getRole(node: IoK8sApiCoreV1Node): string {
+  const roles: string[] = [];
+  if (node.metadata?.labels) {
+    for (const [k, v] of Object.entries(node.metadata.labels)) {
+      if (k.startsWith(labelNodeRolePrefix)) {
+        const role = k.replace(labelNodeRolePrefix, "");
+        if (role) roles.push(role);
+      } else if (k.startsWith(nodeLabelRole)) {
+        roles.push(v);
+      }
+    }
+  }
+  if (roles.length) return roles.join(",");
+  return "<none>";
+}
+
+export function getNodeInternalIP(node: IoK8sApiCoreV1Node): string {
+  if (node.status?.addresses) {
+    for (const addr of node.status?.addresses) {
+      if (addr.type === "InternalIP") {
+        return addr.address;
+      }
+    }
+  }
+  return "<none>";
+}
+
+export function getNodeExternalIP(node: IoK8sApiCoreV1Node): string {
+  if (node.status?.addresses) {
+    for (const addr of node.status?.addresses) {
+      if (addr.type === "ExternalIP") {
+        return addr.address;
+      }
+    }
+  }
+  return "<none>";
+}
 
 export function renderNodeList(
   nodes: IoK8sApiCoreV1Node[],
@@ -9,13 +82,29 @@ export function renderNodeList(
   const body = nodes.map((node) => {
     return [
       node.metadata?.name ?? "<unknown>",
-      node.status?.conditions?.at(-1)?.type ?? "<unknown>",
+      renderNodeStatus(node),
+      getRole(node),
+      node.metadata?.creationTimestamp?.toLocaleString() ?? "<unknown>",
       node.status?.nodeInfo?.kubeletVersion ?? "<unknown>",
-      node.status?.nodeInfo?.operatingSystem ?? "<unknown>",
+      getNodeInternalIP(node),
+      getNodeExternalIP(node),
+      node.status?.nodeInfo?.osImage ?? "<unknown>",
+      node.status?.nodeInfo?.kernelVersion ?? "<unknown>",
       node.status?.nodeInfo?.containerRuntimeVersion ?? "<unknown>",
     ];
   });
-  const header = ["NAME", "STATUS", "VERSION", "OS", "RUNTIME"];
+  const header = [
+    "NAME",
+    "STATUS",
+    "ROLES",
+    "CREATED TIME",
+    "VERSION",
+    "INTERNAL IP",
+    "EXTERNAL IP",
+    "OS IMAGE",
+    "KERNEL VERSION",
+    "RUNTIME",
+  ];
   const table = new Table();
   table.header(header)
     .body(body);
