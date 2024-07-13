8e39295b407a0aeaa73f81064575a0d3201322c3
skanehira <skanehira@users.noreply.github.com>
2024-07-13 00:08:39 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/k8s.vim/k8s.vim/denops/k8s/deps.ts
    [1/12] Looking for releases: https://deno.land/x/denops_std@v6.4.0/mod.ts
    [1/12] Attempting update: https://deno.land/x/denops_std@v6.4.0/mod.ts -> v6.5.1
    [1/12] Update successful: https://deno.land/x/denops_std@v6.4.0/mod.ts -> v6.5.1
    [2/12] Looking for releases: https://deno.land/x/denops_std@v6.4.0/variable/mod.ts
    [2/12] Attempting update: https://deno.land/x/denops_std@v6.4.0/variable/mod.ts -> v6.5.1
    [2/12] Update successful: https://deno.land/x/denops_std@v6.4.0/variable/mod.ts -> v6.5.1
    [3/12] Looking for releases: https://deno.land/x/unknownutil@v3.17.3/mod.ts
    [3/12] Attempting update: https://deno.land/x/unknownutil@v3.17.3/mod.ts -> v3.18.1
    [3/12] Update successful: https://deno.land/x/unknownutil@v3.17.3/mod.ts -> v3.18.1
    [4/12] Looking for releases: https://deno.land/std@0.222.1/testing/asserts.ts
    [4/12] Attempting update: https://deno.land/std@0.222.1/testing/asserts.ts -> 0.224.0
    [4/12] Update successful: https://deno.land/std@0.222.1/testing/asserts.ts -> 0.224.0
    [5/12] Looking for releases: https://deno.land/x/denops_std@v6.4.0/autocmd/mod.ts
    [5/12] Attempting update: https://deno.land/x/denops_std@v6.4.0/autocmd/mod.ts -> v6.5.1
    [5/12] Update successful: https://deno.land/x/denops_std@v6.4.0/autocmd/mod.ts -> v6.5.1
    [6/12] Looking for releases: https://deno.land/x/cliffy@v1.0.0-rc.4/table/mod.ts
    [6/12] Using latest: https://deno.land/x/cliffy@v1.0.0-rc.4/table/mod.ts
    [7/12] Looking for releases: https://deno.land/std@0.222.1/datetime/mod.ts
    [7/12] Attempting update: https://deno.land/std@0.222.1/datetime/mod.ts -> 0.224.0
    [7/12] Update successful: https://deno.land/std@0.222.1/datetime/mod.ts -> 0.224.0
    [8/12] Looking for releases: https://deno.land/x/denops_std@v6.4.0/variable/mod.ts
    [8/12] Attempting update: https://deno.land/x/denops_std@v6.4.0/variable/mod.ts -> v6.5.1
    [8/12] Update successful: https://deno.land/x/denops_std@v6.4.0/variable/mod.ts -> v6.5.1
    [9/12] Looking for releases: https://deno.land/x/denops_std@v6.4.0/batch/mod.ts
    [9/12] Attempting update: https://deno.land/x/denops_std@v6.4.0/batch/mod.ts -> v6.5.1
    [9/12] Update successful: https://deno.land/x/denops_std@v6.4.0/batch/mod.ts -> v6.5.1
    [10/12] Looking for releases: https://deno.land/std@0.222.1/fs/mod.ts
    [10/12] Attempting update: https://deno.land/std@0.222.1/fs/mod.ts -> 0.224.0
    [10/12] Update successful: https://deno.land/std@0.222.1/fs/mod.ts -> 0.224.0
    [11/12] Looking for releases: https://deno.land/std@0.222.1/path/mod.ts
    [11/12] Attempting update: https://deno.land/std@0.222.1/path/mod.ts -> 0.224.0
    [11/12] Update successful: https://deno.land/std@0.222.1/path/mod.ts -> 0.224.0
    [12/12] Looking for releases: https://cdn.skypack.dev/string-width@5.0.1?dts
    [12/12] Skip updating: https://cdn.skypack.dev/string-width@5.0.1?dts
    
    Already latest version:
    https://deno.land/x/cliffy@v1.0.0-rc.4/table/mod.ts == v1.0.0-rc.4
    https://cdn.skypack.dev/string-width@5.0.1?dts == 5.0.1?dts
    
    Successfully updated:
    https://deno.land/x/denops_std@v6.4.0/mod.ts v6.4.0 -> v6.5.1
    https://deno.land/x/denops_std@v6.4.0/variable/mod.ts v6.4.0 -> v6.5.1
    https://deno.land/x/unknownutil@v3.17.3/mod.ts v3.17.3 -> v3.18.1
    https://deno.land/std@0.222.1/testing/asserts.ts 0.222.1 -> 0.224.0
    https://deno.land/x/denops_std@v6.4.0/autocmd/mod.ts v6.4.0 -> v6.5.1
    https://deno.land/std@0.222.1/datetime/mod.ts 0.222.1 -> 0.224.0
    https://deno.land/x/denops_std@v6.4.0/variable/mod.ts v6.4.0 -> v6.5.1
    https://deno.land/x/denops_std@v6.4.0/batch/mod.ts v6.4.0 -> v6.5.1
    https://deno.land/std@0.222.1/fs/mod.ts 0.222.1 -> 0.224.0
    https://deno.land/std@0.222.1/path/mod.ts 0.222.1 -> 0.224.0

--- denops/k8s/deps.ts
+++ denops/k8s/deps.ts
@@ -1,16 +1,16 @@
-export * from "https://deno.land/x/denops_std@v6.4.0/mod.ts";
-export * from "https://deno.land/x/denops_std@v6.4.0/variable/mod.ts";
-export * from "https://deno.land/x/unknownutil@v3.17.3/mod.ts";
+export * from "https://deno.land/x/denops_std@v6.5.1/mod.ts";
+export * from "https://deno.land/x/denops_std@v6.5.1/variable/mod.ts";
+export * from "https://deno.land/x/unknownutil@v3.18.1/mod.ts";
 export {
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.222.1/testing/asserts.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v6.4.0/autocmd/mod.ts";
+} from "https://deno.land/std@0.224.0/testing/asserts.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v6.5.1/autocmd/mod.ts";
 export { default as stringWidth } from "https://cdn.skypack.dev/string-width@5.0.1?dts";
 export { Table } from "https://deno.land/x/cliffy@v1.0.0-rc.4/table/mod.ts";
-export * as datetime from "https://deno.land/std@0.222.1/datetime/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v6.4.0/variable/mod.ts";
-export * from "https://deno.land/x/denops_std@v6.4.0/batch/mod.ts";
-export * as fs from "https://deno.land/std@0.222.1/fs/mod.ts";
-export * as path from "https://deno.land/std@0.222.1/path/mod.ts";
+export * as datetime from "https://deno.land/std@0.224.0/datetime/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v6.5.1/variable/mod.ts";
+export * from "https://deno.land/x/denops_std@v6.5.1/batch/mod.ts";
+export * as fs from "https://deno.land/std@0.224.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.224.0/path/mod.ts";
 
